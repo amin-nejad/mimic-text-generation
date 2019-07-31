6f7abe2bfc7e6865e6d21a8d86f1080b02d44e6d
Ali Amin-Nejad <aanejad@hotmail.co.uk>
2019-07-31 21:22:22 +0000

    t2t training

--- t2t/define_problem.py
+++ t2t/define_problem.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-tf.enable_eager_execution()
+#tf.enable_eager_execution()
 from tensor2tensor.utils import trainer_lib
 RANDOM_SEED = 301
 trainer_lib.set_random_seed(RANDOM_SEED)
