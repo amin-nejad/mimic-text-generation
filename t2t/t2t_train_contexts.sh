a731619b0bc5db0e33a2c69cd09be9c03b77133c
amin-nejad <44096034+amin-nejad@users.noreply.github.com>
2019-08-01 01:56:46 +0100

    Update t2t_train_contexts.sh

--- t2t/t2t_train_contexts.sh
+++ t2t/t2t_train_contexts.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3
-
 BASE=$HOME/project # change this as necessary
 
 PROBLEM=mimic_discharge_summaries
 MODEL=transformer
 HPARAMS=transformer_base
-DATA_DIR=$BASE/data/t2t_experiments/full_context/data
-TRAIN_DIR=$BASE/data/t2t_experiments/full_context/output
 USR_DIR=$BASE/t2t
 
-nohup t2t-trainer \
-    --data_dir=$DATA_DIR \
-    --problem=$PROBLEM \
-    --model=$MODEL \
-    --hparams_set=$HPARAMS \
-    --hparams="max_length=10000,max_target_seq_length=512,max_input_seq_length=512" \
-    --output_dir=$TRAIN_DIR \
-    --t2t_usr_dir=$USR_DIR \
-    --train_steps=4500 \
-    --eval_steps=100 \
-    --local_eval_frequency=250 \
-    --train_steps=5000 \
-    --worker_gpu=4 &
+for i in {h,h-gae,h-gae-d,h-gae-p,h-gae-d-p,h-gae-d-p-m,h-gae-d-p-m-t,h-gae-d-p-m-l}
+do
+
+    DATA_DIR=$BASE/data/t2t_experiments/other_contexts/$i/data
+    TRAIN_DIR=$BASE/data/t2t_experiments/other_contexts/$i/output
+
+    nohup t2t-trainer \
+        --data_dir=$DATA_DIR \
+        --problem=$PROBLEM \
+        --model=$MODEL \
+        --hparams_set=$HPARAMS \
+        --hparams="max_length=10000,max_target_seq_length=512,max_input_seq_length=512" \
+        --output_dir=$TRAIN_DIR \
+        --t2t_usr_dir=$USR_DIR \
+        --train_steps=5000 \
+        --local_eval_frequency=500 \
+        --eval_steps=50 \
+        --worker_gpu=4 &
+done
