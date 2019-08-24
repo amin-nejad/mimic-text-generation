0b72f23dd3dceb9227ed9a8690338e6d342e4a39
Ali Amin-Nejad <aanejad@hotmail.co.uk>
2019-08-24 15:57:27 +0000

    decoding

--- gpt2/03_train.sh
+++ gpt2/03_train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0
 
 DATA=$HOME/project/text-generation/data/gpt2
 
 cd ./gpt-2
 
-PYTHONPATH=src ./train.py --dataset $DATA/input-text.txt.npz --val_dataset $DATA/val-input-text.txt.npz --sample_every=1000 --save_every=1000 --optimizer='adam' --model_name=117M --val_every=200 --batch_size=2
+PYTHONPATH=src ./train.py --dataset $DATA/input-text.txt.npz --val_dataset $DATA/val-input-text.txt.npz --sample_every=10000 --save_every=10000 --optimizer='adam' --model_name=117M --val_every=1000 --batch_size=2
