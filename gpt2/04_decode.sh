3fd77d93e5050e333a7706abc84d886c93355736
Ali Amin-Nejad <aanejad@hotmail.co.uk>
2019-08-25 19:37:39 +0000

    decode

--- gpt2/04_decode.sh
+++ gpt2/04_decode.sh
@@ -10,6 +10,7 @@ INPUT_FILE=$DATA_DIR/test-input-text.txt
 OUTPUT_FILE=$DATA_DIR/test-output-text.txt
 TOP_K=0
 TEMP=1
+BS=1
 
 ln -sf ../../models/117M/encoder.json checkpoint/$MODEL/
 ln -sf ../../models/117M/hparams.json checkpoint/$MODEL/
@@ -17,4 +18,4 @@ ln -sf ../../models/117M/vocab.bpe checkpoint/$MODEL/
 
 ln -sf ../checkpoint/$MODEL/ models/ 
 
-python src/generate_conditional_samples.py --input_file $INPUT_FILE --output_file $OUTPUT_FILE --model_name $MODEL --top_k $TOP_K --temperature $TEMP
+python src/generate_conditional_samples.py --input_file $INPUT_FILE --output_file $OUTPUT_FILE --model_name $MODEL --top_k $TOP_K --temperature $TEMP --batch_size $BS
